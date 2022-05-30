735b205756947361eb5e7912394040bc9788f0e9
Roy Palomino <roy100995@gmail.com>
2022-05-30 18:05:02 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}", "./dist/*", "./img/*"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+}
